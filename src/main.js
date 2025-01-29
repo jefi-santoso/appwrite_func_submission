8c783611f91b43dac690b01ea62111055a1e09ea
jefi-santoso <jefi.santoso@gmail.com>
2025-01-30 08:33:16 +1100

    process.env is not working

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY ?? req.headers['x-appwrite-key']);
+    .setKey('standard_8faff3a756ff55bfa075dbe8a6d7abcf1996642b452215e95fcc8a5f119a4952a51ec5349676b17deb75c142f851ef8cae3b60c1ccd68676dac1ed034b84526c501b8064d672a62c2e4fc59b6c9d00706688a27640b12525a18aa531c87074971f2ea8575462595f91d6b26ed5885ace56b4a9301ca78c05b4d471caa12158df');
     // .setKey(req.headers['x-appwrite-key'] ?? '');
   const db = new Databases(client);
 
