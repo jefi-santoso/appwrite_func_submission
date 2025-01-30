f9f17cfba177a51691dc61adfd7a044cbd564886
jefi-santoso <jefi.santoso@gmail.com>
2025-01-31 09:51:48 +1100

    fix bugs

--- src/main.js
+++ src/main.js
@@ -31,10 +31,10 @@ export default async ({ req, res, log, error }) => {
     const submissionHistoryDoc = await db.createDocument(body.$databaseId, submissionHistoryCollectionId, ID.unique(),
     {
       changed_by_username: 'jsantoso',
-      previous_status: 'prevSubmissionHistoryDoc.documents[0].new_status',
+      previous_status: prevSubmissionHistoryDoc.documents[0].new_status,
       new_status: body.status,
       changed_at: body.$updatedAt,
-      submission: [`'${body.$id}'`],
+      submission: [`${body.$id}`],
     });
     log(submissionHistoryDoc);
   } catch(err) {
